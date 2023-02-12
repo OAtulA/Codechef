3bcc067986bd8c6e06c892e7c842d67f9139b0a6
OAtulA <oraonatula@gmail.com>
2023-02-12 22:31:26 +0530

    Initiating the good learnings.

---
+++ Languages/Java/IO.java
@@ -0,0 +1,12 @@
+// Update the blanks in the code below
+import java.util.Scanner;
+
+class IO
+{
+	public static void main (String[] args)
+	{
+        Scanner objName = new Scanner(System.in);
+        String s = objName.next();
+		System.out.println(s);
+	}
+}
