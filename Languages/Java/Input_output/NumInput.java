56891f5d89017ed7b6eb35d6504e6a26aaeb0957
OAtulA <oraonatula@gmail.com>
2023-02-12 22:44:28 +0530

    Added Numinput.java

---
+++ Languages/Java/Input_output/NumInput.java
@@ -0,0 +1,10 @@
+package Codechef.Languages.Java.Input_output;
+import java.util.Scanner;
+public class NumInput {
+    public static void main (String[] args)
+	{
+        Scanner objName = new Scanner(System.in);
+        int N = objName.nextInt();
+		System.out.println(N);
+	}
+}
