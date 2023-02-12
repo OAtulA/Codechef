3121c66468d8638588d4d9c0885281467e9e433d
OAtulA <oraonatula@gmail.com>
2023-02-12 22:52:45 +0530

    Practising 3 input in one line in java.

---
+++ Languages/Java/Input_output/num3inp.java
@@ -0,0 +1,15 @@
+package Codechef.Languages.Java.Input_output;
+
+import java.util.Scanner;
+
+public class num3inp {
+    public static void main (String[] args)
+	{
+        Scanner objName = new Scanner(System.in);
+        int A = objName.nextInt();
+        int B = objName.nextInt();
+        int C = objName.nextInt();
+		System.out.println(A + " " + B + " " + C);
+	}
+
+}
