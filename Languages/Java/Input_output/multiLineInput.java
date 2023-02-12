a81a8d84e368963561f4f82a9d6accbe51fb5cae
OAtulA <oraonatula@gmail.com>
2023-02-12 22:54:42 +0530

    Adding  multiLineInput.java

---
+++ Languages/Java/Input_output/multiLineInput.java
@@ -0,0 +1,26 @@
+package Codechef.Languages.Java.Input_output;
+
+import java.util.Scanner;
+
+public class multiLineInput {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        /*
+         * You need to write a program which does the following
+         * 
+         * Accepts 22 integers as input in 1st line as the variables AA, BB
+         * Accepts 33 integers as input in the 2nd line as the variables CC, DD and EE
+         * Prints out space-seperated 55 integers as output in a single line to the
+         * console
+         * 
+         * Code the solution in the IDE and then click Submit to continue.
+         */
+
+        int A = read.nextInt();
+        int B = read.nextInt();
+        int C = read.nextInt();
+        int D = read.nextInt();
+        int E = read.nextInt();
+        System.out.print(A + " " + B + " " + C + " " + D + " " + E);
+    }
+}
